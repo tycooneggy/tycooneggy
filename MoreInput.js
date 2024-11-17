c177a6efc5274b22e8afdb80988db0c690f0531c
tycooneggy <tycoon0975356872@gmail.com>
2024-11-17 19:42:36 +0800

    Add files via upload

---
+++ MoreInput.js
@@ -0,0 +1,60 @@
+class MoreInput {
+    constructor(runtime) {
+        this.runtime = runtime
+    } 
+    
+    getInfo() {
+        return {
+            id: "MoreInput“",
+            name: "更多輸入方塊",
+            blocks:[
+            {
+                opcode: "ReturnPi",
+                blockType: Scratch.BlockType.REPORTER,
+                text: "圓周率"
+            },
+            {
+                opcode: "ReturnLetter",
+                blockType: Scratch.BlockType.REPORTER,
+                text: "字串[Letter]",
+                arguments: {
+                    Letter: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultvalue: "apple"
+                    }
+                }
+            },
+            {
+                opcode: "GetLetterIndexRange",
+                blockType: Scratch.BlockType.REPORTER,
+                text: "字串[Letter]的[SN]～[FN]",
+                arguments: {
+                    Letter: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultvalue: "apple"
+                    },
+                    SN: {
+                        type: Scratch.ArgumentType.NUMBER,
+                        defaultvalue: 1
+                    },
+                    FN: {
+                        type: Scratch.ArgumentType.NUMBER 
+                        defaultvalue: 2
+                    }
+                }
+            }
+            ]
+        };
+    }
+    ReturnPi(){
+        return Math.PI
+    }
+    ReturnLetter(args){
+        return args.Letter
+    }
+    GetLetterIndexRange(args){
+        return args.Letter.substring(args.SN, args.FN)
+    }
+}
+
+Scratch.extensions.register(new MoreInput())
